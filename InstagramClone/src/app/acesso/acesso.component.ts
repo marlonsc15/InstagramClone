46324ec6111113df03517635dde945a41277c728
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-11-07 17:05:26 -0300

    componentes inicias

---
+++ InstagramClone/src/app/acesso/acesso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./Acesso.component.css']
+})
+export class AcessoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
