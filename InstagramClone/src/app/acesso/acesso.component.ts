a415bef3387e6a0e35ea59ee2dcce2467ffd34e2
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-11-08 18:44:50 -0300

    animações de imagens

--- InstagramClone/src/app/acesso/acesso.component.ts
+++ InstagramClone/src/app/acesso/acesso.component.ts
@@ -1,11 +1,25 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./Acesso.component.css']
+  styleUrls: ['./Acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px , 0)' }),
+        animate('500ms 0.5s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
 
   constructor() { }
 
